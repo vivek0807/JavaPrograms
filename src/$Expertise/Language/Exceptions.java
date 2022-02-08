70fa77775cb46c38dc8c91f9cde0396fc9eb4075
vivek0807 <35535866+vivek0807@users.noreply.github.com>
2022-02-08 19:45:09 +0530

    Added

--- src/$Expertise/Language/Exceptions.java
+++ src/$Expertise/Language/Exceptions.java
@@ -11,7 +11,7 @@ public class Exceptions {
         }
         catch (ArithmeticException |NullPointerException |ArrayIndexOutOfBoundsException e){   //catches Arithmetic Exception
             if (e.toString().equals("java.lang.NullPointerException"))
-                System.out.println("Kya pagal admi hai bey");
+                System.out.println("Kya pagal admi hai ");
         }
 
     }
