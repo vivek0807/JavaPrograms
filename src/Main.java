5eaf1e19f61aa8896bb5fb1189167dcc41f17d08
Vivek Kumar <35535866+vivek0807@users.noreply.github.com>
2022-09-02 00:59:23 +0530

    Added

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,20 @@
+import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
-   static void reversewords(String s){
 
-       String arr[]=s.split(" ");
-        String str="";
-       for (int i = arr.length-1; i >=0 ; i--) {
-           //System.out.print(arr[i]);
-           str+=arr[i];
-           if(i!=0)
-           str+=".";
-       }
-       //System.out.println(str);
+    void Steamer(){
+
+        List<Integer> list=List.of(1,2,3,4,5,6,7,8,9,0);
+
+        Stream<Integer> stream=list.stream();
+
+        stream.filter(i->i%2==0).forEach(i-> System.out.println(i));
     }
 
     public static void main(String[] args) {
-         reversewords("This is too much");
+    Main main= new Main();
+    main.Steamer();
     }
 
     }
