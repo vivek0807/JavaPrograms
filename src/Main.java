ab8d1062e65a13de12a405f40ca9ef1fc31d441f
Vivek Kumar <vivekk.0807@gmail.com>
2022-10-07 00:39:53 +0530

    Changes

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,26 @@ public class Main {
 
     public static void main(String[] args) {
 
-       List<String> list= Arrays.asList("Helo","Mello","Pello","Khelo");
+     HashMap<String,String> hashMap= new HashMap<>();
+     hashMap.put("a","aloo");
+     hashMap.put("b","tamatar");
+     hashMap.put("c","khira");
+     hashMap.put("d","gajar");
 
-       String str="";
-       list.stream().map(i->i+"  sdfsdf").forEach((String strs)->{
-           strs=str+strs;
-       });
+
+        List<Map.Entry<String,String>> listEntry= new LinkedList<>(hashMap.entrySet());
+
+        Collections.sort(listEntry, new Comparator<Map.Entry<String, String>>() {
+            @Override
+            public int compare(Map.Entry<String, String> o1, Map.Entry<String, String> o2) {
+                return o1.getValue().compareTo(o2.getValue());
+            }
+        });
+
+        Map<String,String> newHasmap= listEntry.stream().collect(Collectors.toMap(i->i.getKey(),i-> i.getValue()));
+
+
+
 
     }
     }
