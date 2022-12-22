90c58b3a61c8866a98f872aa5422e134791b6ada
Vivek Kumar <vivekk.0807@gmail.com>
2022-12-22 13:23:20 +0530

    Create TwoSum.java

---
+++ src/$Expertise/DataStructures/implemented/Problems/TwoSum.java
@@ -0,0 +1,10 @@
+package $Expertise.DataStructures.implemented.Problems;
+/*
+Given an array of integers nums and an integer target,
+return indices of the two numbers such that they add up to target.
+*/
+public class TwoSum {
+    public static void main(String[] args) {
+        
+    }
+}
