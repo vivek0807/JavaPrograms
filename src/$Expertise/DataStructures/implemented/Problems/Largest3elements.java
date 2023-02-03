c371c964deef8ee86dcdc5c42ab1f1af06963670
Vivek Kumar <vivekk.0807@gmail.com>
2023-02-03 17:55:28 +0530

    ADDED

---
+++ src/$Expertise/DataStructures/implemented/Problems/Largest3elements.java
@@ -0,0 +1,28 @@
+package $Expertise.DataStructures.implemented.Problems;
+
+public class Largest3elements {
+    public static void main(String[] args) {
+
+        int arr[]={1,2,3,4,5,6,7,9};
+
+        int third=Integer.MIN_VALUE,second=Integer.MIN_VALUE,first=Integer.MIN_VALUE;
+
+        for (int i = 0; i <arr.length ; i++) {
+
+        if(arr[i]>first){
+            third=second;
+            second=first;
+            first=arr[i];
+        } else if (arr[i]>second) {
+            third=second;
+            second=arr[i];
+
+        } else if (arr[i]>third) {
+            third=arr[i];
+        }
+
+        }
+
+        System.out.println(third+" "+second+" "+first);
+    }
+}
--- src/$Expertise/DataStructures/implemented/Problems/recurssion.java
+++ src/$Expertise/DataStructures/implemented/Problems/recurssion.java
@@ -3,4 +3,19 @@ package $Expertise.DataStructures.implemented.Problems;
 
 
 public class recurssion {
+
+    static int fibonacci(int n){
+
+        if(n==0)
+            return 0;
+        else if (n==1)
+            return 1;
+        else
+            return fibonacci(n-1)+fibonacci(n-2);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(fibonacci(10));
+    }
 }
