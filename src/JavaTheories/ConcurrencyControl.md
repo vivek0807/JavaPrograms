c3b6c6bb3ec497f76d5cc13ce5affe2f7140278b
Vivek Kumar <vivek.kumar01@lowes.com>
2025-08-02 13:17:07 +0530

    Commited Knowledge

---
+++ src/JavaTheories/ConcurrencyControl.md
@@ -0,0 +1,10 @@
+## Concurrency Control in java
+
+#### Ways to create thread
+- implement Runnable
+- extend thread
+- Callable
+  - same as runnable but just that it can return a value
+  - we override the call method
+  - can throw a checked exception
+
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         int[] nums={5,7,1,4};
+        ExecutorService executorService= Executors.newFixedThreadPool(6);
 
     }
 }
